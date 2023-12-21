701e8d4e6ef3a03acd547094648e2c0e06e308e9
DevaWinner <winneraaniekan@gmail.com>
2023-12-21 10:24:13 +0100

    Add show and create actions to reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -13,6 +13,20 @@ module Api
         end
       end
 
+      def show
+
+      end
+
+      def create
+        reservation = Reservation.new(reservation_params)
+
+        if reservation.save
+          render json: ReservationSerializer.new(reservation).serialized_json
+        else
+          render json: {error: reservation.errors.messages}, status: 422
+        end
+      end
+
       def reservation_params
         params.require(:reservation).permit(:user_id, :package_id, :location, :date)
       end
