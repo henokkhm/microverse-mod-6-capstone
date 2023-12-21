09ea000c75231081486ac7b50bb0790108a1169d
DevaWinner <winneraaniekan@gmail.com>
2023-12-21 10:24:58 +0100

    Add index action to ReservationsController

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,7 +2,16 @@ module Api
   module V1
     class ReservationsController < ApplicationController
 
-      
+      def index
+        package = Package.find_by(slug: params[:package_slug])
+
+        if package
+          reservations = Reservation.where(package: package)
+          render json: reservations, each_serializer: ReservationSerializer, status: :ok
+        else
+          render json: { error: 'Package not found' }, status: :not_found
+        end
+      end
 
       def show
 
