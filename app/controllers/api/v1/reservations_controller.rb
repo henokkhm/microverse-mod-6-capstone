4dda2103b629b4ef9b08182582a2b3f3961aeea7
DevaWinner <winneraaniekan@gmail.com>
2023-12-21 10:24:20 +0100

    Add destroy method to reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -27,6 +27,17 @@ module Api
         end
       end
 
+      def destroy
+        reservation = Reservation.find(reservation_params)
+
+        if reservation.destroy
+          head :no_content
+        else
+          render json: {error: reservation.errors.messages}, status: 422
+        end
+      end
+      private
+
       def reservation_params
         params.require(:reservation).permit(:user_id, :package_id, :location, :date)
       end
